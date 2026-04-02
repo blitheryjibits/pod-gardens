06012fedfb904e7e8c6d1983126569e5a10d4222
robert thornton <robert.thornton92@gmail.com>
2026-04-02 14:11:24 +0300

    updated - responsive menu to use input boxes with id and peer-checked to trigger mobile menu to show in place of react state. This component can now be rendered on the server again.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,13 +1,15 @@
-"use client";
-
 // components/layout/Navbar.tsx
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { TextAlignJustify, X } from "lucide-react";
 
 export function Navbar() {
   const pathname = usePathname();
-  const [open, setOpen] = useState(false);
+
+  const linkClass = (href: string) =>
+    pathname === href
+      ? `text-primary text-lg font-bold font-headline tracking-tight`
+      : `font-semibold font-body text-on-surface-variant hover:text-secondary transition-colors duration-300`;
 
   const links = [
     { label: "Home", href: "/" },
@@ -26,46 +28,60 @@ export function Navbar() {
         </span>
 
         {/* Mobile hamburger menu */}
-        <div className="flex flex-row-reverse md:flex-row gap-4">
-          <div className="flex items-middle justify-center md:hidden py-2">
-            <button
-              type="button"
-              onClick={() => setOpen(!open)}
-              className={`text-on-surface-variant text-2xl focus:outline-none
-                    `}
-              aria-label="Toggle navigation"
-            >
-              {open ? "X" : "☰"}
-            </button>
-          </div>
-
-          {/* Menu */}
-          <ul
-            className={`
-                    list-none md:gap-3 lg:gap-5 text-on-surface-variant md:text-md
-                    flex-col w-full md:pl-8 md:pr-2 lg:px-8
-                    md:flex-row md:items-center md:static md:justify-center transition-all duration-700 
-                    ${open ? "max-h-96 " : "hidden md:flex max-h-0 md:max-h-none"}`}
+        <div className="relative h-100% bg-transparent flex-1">
+          <input
+            title="sidebar-active"
+            type="checkbox"
+            name="sidebar-active"
+            id="sidebar-active"
+            className="hidden peer"
+          />
+          <label
+            id=""
+            htmlFor="sidebar-active"
+            className="md:hidden peer-checked:hidden
+          absolute right-2 top-[50%] -translate-y-[50%]"
           >
-            {links.map((link) => (
-              <li key={link.href}>
+            <TextAlignJustify />
+          </label>
+
+          {/* menu list */}
+          <ul
+            className={`absolute top-0 -right-full w-full h-screen
+              flex flex-col gap-4
+              bg-background/60 backdrop-blur-xs 
+              peer-checked:right-0
+              transtition-all duration-300
+              md:max-w-fit md:flex md:flex-row md:right-0 md:h-full
+              md:gap-0 md:pt-0 md:pr-4 md:text-sm
+              
+              `}
+          >
+            <li className="ml-auto md:hidden">
+              <label htmlFor="sidebar-active">
+                <X />
+              </label>
+            </li>
+            {links.map((item) => (
+              <li
+                key={item.href}
+                id={item.label}
+                className="md:flex md:items-center text-accent"
+              >
                 <Link
-                  href={link.href}
-                  className={`${
-                    pathname === link.href
-                      ? `text-primary text-xl border-b-2 border-secondary pb-1 font-bold font-headline italic tracking-tight`
-                      : `font-semibold font-body hover:text-secondary transition-colors duration-300`
-                  }`}
+                  href={item.href}
+                  className={`py-1 px-2 rounded-md block text-center ${linkClass(item.href)}`}
                 >
-                  {link.label}
+                  {item.label}
                 </Link>
               </li>
             ))}
+            <li className="hidden md:flex items-center">
+              <button className="bg-primary text-on-primary text-sm lg:text-md text-nowrap px-3 lg:px-6 py-1 lg:py-2.5 rounded-md lg:font-bold font-semibold hover:scale-110 transition-all active:scale-95 duration-300 ease-in-out">
+                Get Started
+              </button>
+            </li>
           </ul>
-
-          <button className="bg-primary text-on-primary text-sm lg:text-md text-nowrap px-3 lg:px-6 py-1 lg:py-2.5 rounded-md lg:font-bold font-semibold hover:scale-110 transition-all active:scale-95 duration-300 ease-in-out">
-            Get Started
-          </button>
         </div>
       </div>
     </nav>
